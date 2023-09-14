1027137bbc049d05ff73fab157303310be5e5a82
Abdul Ahad Khan <abdulahadkhan@live.com>
2023-09-14 17:13:29 -0400

    Dev CD test

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ import viteLogo from '/logo.svg'
 document.querySelector('#app').innerHTML = `
   <div>
     <img src="${viteLogo}" class="logo" alt="Abdul Ahad Khan logo" />
-    <h1>Abdul Ahad Khan</h1>
+    <h1>Abdul Ahad Khan (dev)</h1>
   </div>
 `
